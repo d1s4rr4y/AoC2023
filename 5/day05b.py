2820c271bf2ae34b3b6c30199f8af962576e333a
Theumbrellaone <ellagrobinson04@gmail.com>
2023-12-09 23:06:18 +0000

    day 5 updated

--- 5/day05b.py
+++ 5/day05b.py
@@ -1,44 +1,39 @@
 import re
 
 with open('day05.txt', 'r') as f:
-    input_data = f.read().splitlines()
+    inputDat = f.read().splitlines()
 
 maps = []
-for line in input_data[2:]:
+for line in inputDat[2:]:
     if 'map' in line:
         maps.append(dict())
     elif line != '':
         destination, source, length = [int(value) for value in line.split()]
-        maps[-1][range(source, source+length)] \
+        maps[-1][range(source, source + length)] \
             = range(destination, destination+length)
 
-
-# The strategy used here is an improvement over a naive brute force approach,
-# but can be optimized by checking whole ranges at a time (while taking
-# into account where they 'split')
-
-def reverse_lookup_seed(location: int) -> int:
+def reverseLookupSeed(location: int) -> int:
     value = location
-    for current_map in reversed(maps):
+    for curMap in reversed(maps):
         value = next(
-            (source_range.start + (value - destination_range.start)
-             for source_range, destination_range in current_map.items()
-             if value in destination_range),
-            value  # fallback
+            (srcRange.start + (value - destRange.start)
+             for srcRange, destRange in curMap.items()
+             if value in destRange),
+            value
         )
     return value
 
 
-initial_seed_data = [int(seed) for seed in re.findall(r'\d+', input_data[0])]
-seed_ranges = []
-for index in range(0, len(initial_seed_data) - 1, 2):
-    start, length = initial_seed_data[index:index+2]
-    seed_ranges.append(range(start, start+length))
+initSeedDat = [int(seed) for seed in re.findall(r'\d+', inputDat[0])]
+seedRanges = []
+for index in range(0, len(initSeedDat) - 1, 2):
+    start, length = initSeedDat[index:index + 2]
+    seedRanges.append(range(start, start + length))
 
 location = 0
 while True:
-    potential_seed = reverse_lookup_seed(location)
-    if any(potential_seed in seed_range for seed_range in seed_ranges):
+    potentialSeed = reverseLookupSeed(location)
+    if any(potentialSeed in seedRange for seedRange in seedRanges):
         print(location)
         break
     location += 1
